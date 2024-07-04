0ef36a8c574ad36a8bf1d6ea11253d7b018b8d81
Erik Roth <erikroth@isu.edu>
2024-07-04 12:27:09 -0600

    add additional server commands

--- renderer.js
+++ renderer.js
@@ -2,12 +2,24 @@ const { ipcRenderer } = require('electron')
 
 document.getElementById('startServer').addEventListener('click', () => {
   ipcRenderer.send('start-server')
-})
+});
 
 document.getElementById('stopServer').addEventListener('click', () => {
   ipcRenderer.send('stop-server')
-})
+});
+
+ipcRenderer.on('server-started', () => {
+  document.getElementById('status').textContent = 'Server started';
+});
+
+ipcRenderer.on('server-stopped', () => {
+  document.getElementById('status').textContent = 'Server stopped';
+});
 
 ipcRenderer.on('server-status', (event, status) => {
   document.getElementById('status').innerText = status
-})
+});
+
+ipcRenderer.on('server-error', (event, error) => {
+  document.getElementById('status').innerText = error
+});
